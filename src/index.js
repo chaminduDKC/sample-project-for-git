7aa09b8c4eb3957929c1b7a8c88bb4839ebaf4db
ChaminduDKC <chamindud061@gmail.com>
2025-10-03 14:30:50 +0530

    contact page created

--- src/index.js
+++ src/index.js
@@ -12,6 +12,12 @@ app.get("/about", (req, res) => {
     res.send("About Page");
 });
 
+// contact branch
+app.get("/contact", (req, res) => {
+    res.send("Contact Page");
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
